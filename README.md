29f4d1490d8bc5aa7ea89fac9917bcd0ee2f7eed
clarekwon2000 <clarekwon2000@gmail.com>
2022-02-21 13:15:05 -0500

    Assemble Running Total

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-# shopping-cart
+# Shopping Cart Project 
 
 ## Usage 
+
+Run the program: 
 
 ''' sh 
 python shopping_cart.py 
 '''
+
 
--- shopping_cart.py
+++ shopping_cart.py
@@ -36,38 +36,59 @@ def to_usd(my_price):
     """
     return f"${my_price:,.2f}" #> $12,000.71
 
+total_price = 0
 
-# TODO: write some Python code here to produce the desired output
+while True: 
+
+    # ASK FOR USER INPUT 
+
+    product_id = input("Please input a product identifier, or 'DONE' if there are no more items: ")
+    # print(product_id) #> 9
+    # print(type(product_id))
+
+    if product_id == "DONE":
+        break
+    else:
+        # LOOK UP CORRESPONDING PRODUCTS 
+
+        # print product that has an id attribute equal to "9"
+
+        matching_products = []
+
+        for x in products: 
+            #if x == 3: 
+            #    ___.append(x)
+            # print(x)
+            # print(x["id"])
+            if str(x["id"]) == str(product_id): 
+                # this is a match 
+                matching_products.append(x)
+
+        # print(matching_products)
+        # print(type(matching_products))
+        # print(len(matching_products))
+
+        # print the name of the matching product
+
+        matching_product = matching_products[0]
+        total_price = total_price + matching_product["price"]
+
+        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+
+#
+# INFO DISPLAY / OUTPUT 
+# 
 
 
-
-# ASK FOR USER INPUT 
-
-product_id = input("Please input a product identifier: ")
-print(product_id) #> 9
-print(type(product_id))
+print("TOTAL PRICE: " + str(total_price))
 
 
-# LOOK UP CORRESPONDING PRODUCTS 
+# A grocery store name of your choice
+# A grocery store phone number and/or website URL and/or address of choice
+# The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2020-02-07 03:54 PM)
+# The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $3.50, etc.)
+# The total cost of all shopping cart items (i.e. the "subtotal"), formatted as US dollars and cents (e.g. $19.47), calculated as the sum of their prices
+# The amount of tax owed (e.g. $1.70), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
+# The total amount owed, formatted as US dollars and cents (e.g. $21.17), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
+# A friendly message thanking the customer and/or encouraging the customer to shop again
 
-# print product that has an id attribute equal to "9"
-
-matching_products = []
-
-for x in products: 
-    #if x == 3: 
-    #    ___.append(x)
-    # print(x)
-    # print(x["id"])
-    if str(x["id"]) == str(product_id): 
-        # this is a match 
-        matching_products.append(x)
-
-print(matching_products)
-print(type(matching_products))
-print(len(matching_products))
-
-# print the name of the matching product
-
-matching_product = matching_products[0]
-print(matching_product["name"]), matching_product["price"]
